dd86d94f5c7a018abb91ceda509d05195ae67b25
Shamaan3066 <151059420+Shamaan3066@users.noreply.github.com>
2024-05-30 01:16:06 +0530

    Update ApiRequest.js

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,4 +1,4 @@
-export const host = 'http://localhost:5001'; //add web server here
+export const host = 'taskapp-backend.vercel.app'; //add web server here
 export const registerAPI = `${host}/auth/register`;
 export const loginAPI = `${host}/auth/login`;
 export const taskApi = `${host}/tasks`
