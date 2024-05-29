8ee2daa9077c267412d059465bf885196ac2cc41
Shamaan3066 <151059420+Shamaan3066@users.noreply.github.com>
2024-05-30 01:26:59 +0530

    Update ApiRequest.js

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,4 +1,4 @@
-export const host = ''; //add web server here
+export const host = 'https://taskapp-hbyf.onrender.com'; //add web server here
 export const registerAPI = `${host}/auth/register`;
 export const loginAPI = `${host}/auth/login`;
 export const taskApi = `${host}/tasks`
